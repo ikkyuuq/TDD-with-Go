4e84bcc99a3c796216ec944f6852eaa32515e29a
ikkyuuq <kittipongprasompong@gmail.com>
2024-07-29 05:11:39 +0700

    feat: more on io.Writer with internet example

--- dependencies-injection/DI.go
+++ dependencies-injection/DI.go
@@ -3,9 +3,23 @@ package dependenciesinjection
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 // logMessage can write to any io.Writer without modification
 func logMessage(w io.Writer, level, msg string) {
 	fmt.Fprintf(w, "[%s] %s", level, msg)
+}
+
+func LogMessageHandler(w http.ResponseWriter, req *http.Request) {
+	msg := req.URL.Query().Get("message")
+	level := req.URL.Query().Get("level")
+	if msg == "" {
+		http.Error(w, "Missing message parameter", http.StatusBadRequest)
+		return
+	}
+	if level == "" {
+		level = "INFO"
+	}
+	logMessage(w, level, msg)
 }
--- dependencies-injection/DI_test.go
+++ dependencies-injection/DI_test.go
@@ -4,21 +4,61 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
 
 func TestLogMessage(t *testing.T) {
-	// bytes.Buffer which implements the io.Writer interface to capture the output.
-	buffer := bytes.Buffer{}
-	logMessage(&buffer, "DEBUG", "This is a debug message")
+	t.Run("capture the output with bytes.Buffer", func(t *testing.T) {
+		// bytes.Buffer which implements the io.Writer interface to capture the output.
+		buffer := bytes.Buffer{}
+		logMessage(&buffer, "DEBUG", "This is a debug message")
 
-	got := buffer.String()
-	want := "[DEBUG] This is a debug message"
+		got := buffer.String()
+		want := "[DEBUG] This is a debug message"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("the internet", func(t *testing.T) {
+		// Create new http server with httptest that take hanlderfunc with LogMessageHandler
+		server := httptest.NewServer(http.HandlerFunc(LogMessageHandler))
+		// Close server
+		defer server.Close()
+		// Mockup url for testing with server.URL following with query
+		url := fmt.Sprintf("%s?level=INFO&message=This+a+INFO+message+from+internet", server.URL)
+
+		// GET on specific URL
+		resp, err := http.Get(url)
+		if err != nil {
+			t.Fatalf("Failed to make a request: %v", err)
+		}
+		// Close resp after reading from it
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status OK, got %v", resp.StatusCode)
+		}
+
+		// Create a recorder that take body field
+		body := httptest.NewRecorder().Body
+		// Read the response body
+		body.ReadFrom(resp.Body)
+		if err != nil {
+			t.Fatalf("Failed to read response body: %v", err)
+		}
+
+		// Get a string from *bytes.Buffer (body) that read body of resp
+		got := body.String()
+		want := "[INFO] This a INFO message from internet"
+
+		if got != want {
+			t.Errorf("got %q want %q", server.URL, want)
+		}
+	})
 }
 
 func ExampleLogMessage_info() {
@@ -43,4 +83,20 @@ func ExampleLogMessage_file() {
 	content, _ := io.ReadAll(f)
 	fmt.Print(string(content))
 	// Output: [ERROR] This is just a error message
+}
+
+func ExmapleLogMessage_internet() {
+	server := httptest.NewServer(http.HandlerFunc(LogMessageHandler))
+	defer server.Close()
+
+	url := fmt.Sprintf("%s?level=INFO&message=This+a+INFO+message+from+internet", server.URL)
+	resp, _ := http.Get(url)
+	resp.Body.Close()
+
+	body := httptest.NewRecorder().Body
+	body.Reset()
+	body.ReadFrom(resp.Body)
+
+	fmt.Println(body.String())
+	// Output: [INFO] This a INFO message from internet
 }
